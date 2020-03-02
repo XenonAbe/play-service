2afc4adcbd80c66bfa1f5758f7aa31c9bc0293d2
abe <xenon.abe@gmail.com>
2020-03-02 15:49:54 +0900

    Visual Studio 2019 でプロジェクトを再構成

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,33 +4,33 @@ using System.Runtime.InteropServices;
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
-// これらの属性値を変更してください。
+// これらの属性値を変更します。
 [assembly: AssemblyTitle("PlayService")]
 [assembly: AssemblyDescription("Run Play Framework 2 application as a windows service.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PlayService")]
-[assembly: AssemblyCopyright("Copyright ©  2014-2019")]
+[assembly: AssemblyCopyright("Copyright ©  2014-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
-// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
-// その型の ComVisible 属性を true に設定してください。
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定します。
 [assembly: ComVisible(false)]
 
-// 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
 [assembly: Guid("5cf8df09-6fb5-452b-85ea-c9fdc865ae26")]
 
-// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
 //
-// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.2.0")]
-[assembly: AssemblyFileVersion("1.2.2.0")]
+[assembly: AssemblyVersion("1.2.3.0")]
+[assembly: AssemblyFileVersion("1.2.3.0")]
